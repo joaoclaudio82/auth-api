24f50d3ef6917bd0ea10e1f34c50e52f6ee43def
joaoclaudio82 <joaoclaudio82@gmail.com>
2026-06-12 16:19:05 -0300

    cria o database.py

---
+++ app/core/database.py
@@ -0,0 +1,25 @@
+import os
+
+from sqlalchemy import create_engine #create_engine do SQLAlchemy. Ele é usado para criar uma conexão com o banco de dados usando a URL fornecida.
+#sessionmaker do SQLAlchemy. Ele é usado para criar uma classe de sessão que pode ser usada para interagir com o banco de dados.
+from sqlalchemy.orm import DeclarativeBase, declarative_base, sessionmaker
+#declarative_base do SQLAlchemy. Ele é usado para criar uma classe base para os modelos de banco de dados, permitindo que eles sejam definidos como classes Python normais.
+from app.core.config import settings
+
+#Importa o objeto settings do módulo app.core.config. Ele contém as configurações do projeto, como a URL do banco de dados, chaves de API, etc.
+engine = create_engine(settings.database_url)
+
+#Cria um objeto engine usando a URL do banco de dados definida em settings.database_url. Este objeto é usado para se conectar ao banco de dados.
+SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
+
+def get_db():
+    """Dependência do FastAPI:abre sessão por requisição e fecha no fim."""
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
