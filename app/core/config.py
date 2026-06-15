60c189a9fb863d540cd919aa33ac711462c5c99c
joaoclaudio82 <joaoclaudio82@gmail.com>
2026-06-14 21:15:11 -0300

    fix: ajustes no config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,13 @@
 #aqui iremos colocar as configurações do projeto, como por exemplo a url do banco de dados, chaves de api, etc.
 #BaseSettings é uma classe do Pydantic usada para criar classes de configuração que carregam valores automaticamente de:
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    database_url: str = "postgresql+psycopg2://app:app@localhost:5433/appdb"
-    secret_key: str = "troque-em-producao"
+    database_url: str 
+    secret_key: str 
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env")
 #Cria um objeto settings com os valores definidos em Settings
 settings = Settings()
